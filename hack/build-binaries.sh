926af35e7d90680b9295e77c976c4638856c794d
dmitriy kalinin <dkalinin@pivotal.io>
2019-03-18 14:35:45 -0700

    add .exe for windows binaries

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -4,6 +4,6 @@ set -e -x -u
 
 GOOS=darwin GOARCH=amd64 go build -o ytt-darwin-amd64 ./cmd/ytt
 GOOS=linux GOARCH=amd64 go build -o ytt-linux-amd64 ./cmd/ytt
-GOOS=windows GOARCH=amd64 go build -o ytt-windows-amd64 ./cmd/ytt
+GOOS=windows GOARCH=amd64 go build -o ytt-windows-amd64.exe ./cmd/ytt
 
-shasum -a 256 ./ytt-*-amd64
+shasum -a 256 ./ytt-*-amd64*
