58ea072ae9237f2bafc2b2067a6cc91e0f8ed9a2
Dmitriy Kalinin <cppforlife@gmail.com>
2019-04-23 17:24:25 -0700

    GOCACHE is not supported by go1.12

--- hack/test-unit.sh
+++ hack/test-unit.sh
@@ -4,6 +4,8 @@ set -e -x
 
 go fmt github.com/k14s/ytt/...
 
-GOCACHE=off go test -v `go list ./...|grep -v yaml.v2` "$@"
+go clean -testcache
+
+go test -v `go list ./...|grep -v yaml.v2` "$@"
 
 echo UNIT SUCCESS
