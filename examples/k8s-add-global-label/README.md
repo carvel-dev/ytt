fb4fa236fc3fd09455151e76aaf0ea03c0f90d11
Dmitriy Kalinin <dkalinin@pivotal.io>
2019-10-30 11:35:07 -0400

    add example showing how to add k8s label everywhere

---
+++ examples/k8s-add-global-label/README.md
@@ -0,0 +1,5 @@
+This example was created to show how to add a label to all resources (using overlays) with its value injected from outside (https://kubernetes.slack.com/archives/C9A5ALABG/p1572421415180100).
+
+```bash
+ytt -f . -v build_num=123
+```
--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -31,6 +31,7 @@ done
 ./ytt -f examples/overlay-files > /dev/null
 ./ytt -f examples/overlay-regular-files --file-mark file.yml:type=yaml-plain > /dev/null
 
+diff <(./ytt -f examples/k8s-add-global-label)        examples/k8s-add-global-label/expected.txt
 diff <(./ytt -f examples/k8s-adjust-rbac-version)     examples/k8s-adjust-rbac-version/expected.txt
 diff <(./ytt -f examples/k8s-docker-secret)           examples/k8s-docker-secret/expected.txt
 diff <(./ytt -f examples/k8s-relative-rolling-update) examples/k8s-relative-rolling-update/expected.txt
