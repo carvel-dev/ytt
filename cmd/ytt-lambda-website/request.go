01e2421ff04a2fa22c634b7cb3eedf04e054d7aa
Garrett Cheadle <gcheadle@vmware.com>
2020-08-11 16:40:44 -0700

    Add copyright to source files
    Signed-off-by: Eli Wrenn <ewrenn@pivotal.io>

--- cmd/ytt-lambda-website/request.go
+++ cmd/ytt-lambda-website/request.go
@@ -1,3 +1,6 @@
+// Copyright 2020 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 package main
 
 import (
--- pkg/yamlmeta/deep_copy.go
+++ pkg/yamlmeta/deep_copy.go
@@ -1,3 +1,6 @@
+// Copyright 2020 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 package yamlmeta
 
 func (n *DocumentSet) DeepCopyAsNode() Node { return n.DeepCopy() }
