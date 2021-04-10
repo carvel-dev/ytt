8b30133b89e17f626c276b250ca0beb93b2bb355
jmcshane <jmcshan1@gmail.com>
2021-04-09 21:06:08 -0500

    arm64 build support

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -9,7 +9,9 @@ repro_flags="-ldflags=-buildid= -trimpath"
 ./hack/build.sh
 
 GOOS=darwin GOARCH=amd64 go build $repro_flags -o ytt-darwin-amd64 ./cmd/ytt
+GOOS=darwin GOARCH=arm64 go build $repro_flags -o ytt-darwin-arm64 ./cmd/ytt
 GOOS=linux GOARCH=amd64 go build $repro_flags -o ytt-linux-amd64 ./cmd/ytt
+GOOS=linux GOARCH=arm64 go build $repro_flags -o ytt-linux-arm64 ./cmd/ytt
 GOOS=windows GOARCH=amd64 go build $repro_flags -o ytt-windows-amd64.exe ./cmd/ytt
 
-shasum -a 256 ./ytt-*-amd64*
+shasum -a 256 ./ytt-*-*
