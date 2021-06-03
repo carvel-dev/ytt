95a101207014eef372e9580fa6f24f68572729fa
David McClure <dmcclure@vmware.com>
2021-06-03 18:00:39 +0000

    Add indent with multiline to json.encode
    - allow json.encode to receive an indent named argument:
      ```
      json.encode('{"a":"b"}', indent=2)
      ```
    - indent greater than 0 indicates the json shall be rendered as a
      multiline string indented with the specified number of spaces
    - indent=0 and omitting indent both result in a single line json string
    - errors are returned if indent > 4 or if arguments other than indent
      are included
    
    Issue #410
    
    Co-authored-by: Tyler Schultz <tschultz@vmware.com>

--- pkg/template/core/args.go
+++ pkg/template/core/args.go
@@ -4,6 +4,8 @@
 package core
 
 import (
+	"fmt"
+
 	"github.com/k14s/starlark-go/starlark"
 )
 
@@ -18,4 +20,30 @@ func BoolArg(kwargs []starlark.Tuple, keyToFind string) (bool, error) {
 		}
 	}
 	return false, nil
+}
+
+func Int64Arg(kwargs []starlark.Tuple, keyToFind string) (int64, error) {
+	for _, arg := range kwargs {
+		key, err := NewStarlarkValue(arg.Index(0)).AsString()
+		if err != nil {
+			return 0, err
+		}
+		if key == keyToFind {
+			return NewStarlarkValue(arg.Index(1)).AsInt64()
+		}
+	}
+	return 0, nil
+}
+
+func CheckArgNames(kwargs []starlark.Tuple, validKeys map[string]struct{}) error {
+	for _, arg := range kwargs {
+		key, err := NewStarlarkValue(arg.Index(0)).AsString()
+		if err != nil {
+			return err
+		}
+		if _, ok := validKeys[key]; !ok {
+			return fmt.Errorf("invalid argument name: %s", key)
+		}
+	}
+	return nil
 }
