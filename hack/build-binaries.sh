0daec8963dcdaa38971d0f7d2b7f3174fb5a77de
Cari <cdean@pivotal.io>
2021-07-12 12:18:03 -0700

    Update workflow to generate website content
    Signed-off-by: Dennis Leon <leonde@vmware.com>

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -16,10 +16,12 @@ go mod tidy
 export CGO_ENABLED=0
 LDFLAGS="-X github.com/k14s/ytt/pkg/version.Version=$VERSION -buildid="
 
+./hack/build.sh $VERSION # Used to generate website/generated.go used by ytt website
+
 GOOS=darwin GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-darwin-amd64 ./cmd/ytt
 GOOS=darwin GOARCH=arm64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-darwin-arm64 ./cmd/ytt
 GOOS=linux GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-linux-amd64 ./cmd/ytt
 GOOS=linux GOARCH=arm64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-linux-arm64 ./cmd/ytt
 GOOS=windows GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-windows-amd64.exe ./cmd/ytt
 
-shasum -a 256 ./ytt-darwin-amd64 ./ytt-darwin-arm64 ./ytt-linux-amd64 ./ytt-linux-arm64 ./ytt-windows-amd64.exe
+shasum -a 256 ./ytt-darwin-amd64 ./ytt-darwin-arm64 ./ytt-linux-amd64 ./ytt-linux-arm64 ./ytt-windows-amd64.exe > ./go-checksums
