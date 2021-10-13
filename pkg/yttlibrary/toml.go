5214511d0771fea515423cc7a92e7aec53463b1b
Stéphane Este-Gracias <sestegra@gmail.com>
2021-10-13 19:33:17 +0200

    Add toml library
    Signed-off-by: Stéphane Este-Gracias <sestegra@gmail.com>

---
+++ pkg/yttlibrary/toml.go
@@ -0,0 +1,89 @@
+// Copyright 2021 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package yttlibrary
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/k14s/starlark-go/starlark"
+	"github.com/k14s/starlark-go/starlarkstruct"
+	"github.com/k14s/ytt/pkg/orderedmap"
+	"github.com/k14s/ytt/pkg/template/core"
+	"github.com/k14s/ytt/pkg/yamlmeta"
+)
+
+var (
+	TOMLAPI = starlark.StringDict{
+		"toml": &starlarkstruct.Module{
+			Name: "toml",
+			Members: starlark.StringDict{
+				"encode": starlark.NewBuiltin("toml.encode", core.ErrWrapper(tmùModule{}.Encode)),
+				"decode": starlark.NewBuiltin("toml.decode", core.ErrWrapper(tmùModule{}.Decode)),
+			},
+		},
+	}
+	TOMLKWARGS = map[string]struct{}{
+		"indent": struct{}{},
+	}
+)
+
+type tmùModule struct{}
+
+func (b tmùModule) Encode(thread *starlark.Thread, f *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	if args.Len() != 1 {
+		return starlark.None, fmt.Errorf("expected exactly one argument")
+	}
+	if err := core.CheckArgNames(kwargs, TOMLKWARGS); err != nil {
+		return starlark.None, err
+	}
+
+	val, err := core.NewStarlarkValue(args.Index(0)).AsGoValue()
+	if err != nil {
+		return starlark.None, err
+	}
+	val = orderedmap.Conversion{yamlmeta.NewGoFromAST(val)}.AsUnorderedStringMaps()
+
+	indent, err := core.Int64Arg(kwargs, "indent")
+	if err != nil {
+		return starlark.None, err
+	}
+
+	var buffer bytes.Buffer
+	encoder := toml.NewEncoder(&buffer)
+	if indent > 0 {
+		encoder.Indent = strings.Repeat(" ", int(indent))
+	}
+	err = encoder.Encode(val)
+
+	if err != nil {
+		return starlark.None, err
+	}
+
+	return starlark.String(buffer.String()), nil
+}
+
+func (b tmùModule) Decode(thread *starlark.Thread, f *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	if args.Len() != 1 {
+		return starlark.None, fmt.Errorf("expected exactly one argument")
+	}
+
+	valEncoded, err := core.NewStarlarkValue(args.Index(0)).AsString()
+	if err != nil {
+		return starlark.None, err
+	}
+
+	var valDecoded interface{}
+
+	err = toml.Unmarshal([]byte(valEncoded), &valDecoded)
+	if err != nil {
+		return starlark.None, err
+	}
+
+	valDecoded = orderedmap.Conversion{valDecoded}.FromUnorderedMaps()
+
+	return core.NewGoValue(valDecoded).AsStarlarkValue(), nil
+}
