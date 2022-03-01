6b1184d00ef9d7d4d5194a91cb0c27ceb69de782
Varsha Munishwar <vmunishwar@vmware.com>
2022-03-01 12:51:43 -0700

    add header and function comment

--- pkg/files/stdin.go
+++ pkg/files/stdin.go
@@ -1,3 +1,6 @@
+// Copyright 2020 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 package files
 
 import (
@@ -8,6 +11,7 @@ import (
 
 var hasStdinBeenRead bool
 
+//ReadStdin only read stdin once
 func ReadStdin() ([]byte, error) {
 	if hasStdinBeenRead {
 		return nil, fmt.Errorf("Standard input has been already read once (has the '-' argument been used in more than one flags?)")
