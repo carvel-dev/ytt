5b89458503e15651b2db99789119cdad66f14e8b
John Ryan <jtigger@infosysengr.com>
2022-04-29 11:14:23 -0700

    Introduce feature flags to control experiments

--- cmd/ytt/ytt.go
+++ cmd/ytt/ytt.go
@@ -7,14 +7,17 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 
 	uierrs "github.com/cppforlife/go-cli-ui/errors"
 	"github.com/vmware-tanzu/carvel-ytt/pkg/cmd"
+	"github.com/vmware-tanzu/carvel-ytt/pkg/feature"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
+	enableExperimentalFeatures()
 
 	command := cmd.NewDefaultYttCmd()
 
@@ -22,5 +25,14 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ytt: Error: %s\n", uierrs.NewMultiLineError(err))
 		os.Exit(1)
+	}
+}
+
+func enableExperimentalFeatures() {
+	if experiments, isSet := os.LookupEnv("YTTEXPERIMENTS"); isSet {
+		for _, experiment := range strings.Split(experiments, ",") {
+			feature.Flags().Enable(experiment)
+			fmt.Fprintf(os.Stderr, "Experimental feature %q enabled.\n", experiment)
+		}
 	}
 }
