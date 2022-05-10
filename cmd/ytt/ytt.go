88f4e26765afc472d518688cbac7bf489a05819c
John Ryan <jtigger@infosysengr.com>
2022-05-10 16:30:54 -0700

    Only allow enabling experiments through env var
    - rename package from "feature" to "experiments"
    - report enabled experiments in "version" command output

--- cmd/ytt/ytt.go
+++ cmd/ytt/ytt.go
@@ -7,17 +7,14 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"strings"
 	"time"
 
 	uierrs "github.com/cppforlife/go-cli-ui/errors"
 	"github.com/vmware-tanzu/carvel-ytt/pkg/cmd"
-	"github.com/vmware-tanzu/carvel-ytt/pkg/feature"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	enableExperimentalFeatures()
 
 	command := cmd.NewDefaultYttCmd()
 
@@ -25,15 +22,5 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ytt: Error: %s\n", uierrs.NewMultiLineError(err))
 		os.Exit(1)
-	}
-}
-
-func enableExperimentalFeatures() {
-	if experiments, isSet := os.LookupEnv("YTTEXPERIMENTS"); isSet && experiments != "" {
-		// if we didn't ignore empty string, we'd attempt to Enable(""); that would panic (rather than ignore).
-		for _, experiment := range strings.Split(experiments, ",") {
-			feature.Flags().Enable(experiment)
-			fmt.Fprintf(os.Stderr, "Experimental feature %q enabled.\n", experiment)
-		}
 	}
 }
