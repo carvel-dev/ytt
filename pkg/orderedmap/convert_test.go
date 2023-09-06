67b77c9395efcf02bf27fd0af9776d7fbdc5ffeb
notoriaga <smeyer@fastmail.com>
2023-09-06 10:59:35 -0700

    don't modify input when converting to orderedmap
    Signed-off-by: notoriaga <smeyer@fastmail.com>

---
+++ pkg/orderedmap/convert_test.go
@@ -0,0 +1,23 @@
+package orderedmap_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/vmware-tanzu/carvel-ytt/pkg/orderedmap"
+)
+
+func TestFromUnorderedMaps(t *testing.T) {
+	inputA := map[string]interface{}{
+		"key": []interface{}{map[string]interface{}{"nestedKey": "nestedValue"}},
+	}
+	inputB := map[string]interface{}{
+		"key": []interface{}{map[string]interface{}{"nestedKey": "nestedValue"}},
+	}
+
+	orderedmap.Conversion{Object: inputA}.FromUnorderedMaps()
+
+	if !reflect.DeepEqual(inputA, inputB) {
+		t.Errorf("Nested object was modified. Got: %v, Expected: %v", inputA, inputB)
+	}
+}
