d858837d838b5d991c609071d0711b463559e0c5
Mathias Petermann <mathias.petermann@gmail.com>
2024-03-28 08:14:04 +0100

    Add tests for json schema
    start refactoring

--- pkg/schema/jsonschema.go
+++ pkg/schema/jsonschema.go
@@ -12,12 +12,12 @@ import (
 
 // JSONSchemaDocument holds the document type used for creating an JSON Schema document
 type JSONSchemaDocument struct {
-	docType *DocumentType
+	OpenAPIDocument
 }
 
 // NewJSONSchemaDocument creates an instance of an OpenAPIDocument based on the given DocumentType
 func NewJSONSchemaDocument(docType *DocumentType) *JSONSchemaDocument {
-	return &JSONSchemaDocument{docType}
+	return &JSONSchemaDocument{*NewOpenAPIDocument(docType)}
 }
 
 // AsDocument generates a new AST of this OpenAPI v3.0.x document, populating the `schemas:` section with the
@@ -43,7 +43,7 @@ func (j *JSONSchemaDocument) calculateProperties(schemaVal interface{}) *yamlmet
 		return j.calculateProperties(typedValue.GetValueType())
 	case *MapType:
 		var items openAPIKeys
-		items = append(items, j.collectDocumentation(typedValue)...)
+		items = append(items, collectDocumentation(typedValue)...)
 		items = append(items, &yamlmeta.MapItem{Key: typeProp, Value: &yamlmeta.Array{Items: []*yamlmeta.ArrayItem{{Value: "object"}}}})
 		items = append(items, &yamlmeta.MapItem{Key: additionalPropsProp, Value: false})
 
@@ -58,7 +58,7 @@ func (j *JSONSchemaDocument) calculateProperties(schemaVal interface{}) *yamlmet
 		return &yamlmeta.Map{Items: items}
 	case *ArrayType:
 		var items openAPIKeys
-		items = append(items, j.collectDocumentation(typedValue)...)
+		items = append(items, collectDocumentation(typedValue)...)
 
 		items = append(items, &yamlmeta.MapItem{Key: typeProp, Value: &yamlmeta.Array{Items: []*yamlmeta.ArrayItem{{Value: "array"}}}})
 		items = append(items, &yamlmeta.MapItem{Key: defaultProp, Value: typedValue.GetDefaultValue()})
@@ -71,10 +71,10 @@ func (j *JSONSchemaDocument) calculateProperties(schemaVal interface{}) *yamlmet
 		return &yamlmeta.Map{Items: items}
 	case *ScalarType:
 		var items openAPIKeys
-		items = append(items, j.collectDocumentation(typedValue)...)
+		items = append(items, collectDocumentation(typedValue)...)
 		items = append(items, &yamlmeta.MapItem{Key: defaultProp, Value: typedValue.GetDefaultValue()})
 
-		typeString := j.jsonSchemaTypeFor(typedValue)
+		typeString := j.openAPITypeFor(typedValue)
 
 		items = append(items, &yamlmeta.MapItem{Key: typeProp, Value: &yamlmeta.Array{Items: []*yamlmeta.ArrayItem{{Value: typeString}}}})
 
@@ -88,7 +88,7 @@ func (j *JSONSchemaDocument) calculateProperties(schemaVal interface{}) *yamlmet
 		return &yamlmeta.Map{Items: items}
 	case *NullType:
 		var items openAPIKeys
-		items = append(items, j.collectDocumentation(typedValue)...)
+		items = append(items, collectDocumentation(typedValue)...)
 
 		properties := j.calculateProperties(typedValue.GetValueType())
 		// we need to append the "null" type to the list of types
@@ -108,7 +108,7 @@ func (j *JSONSchemaDocument) calculateProperties(schemaVal interface{}) *yamlmet
 		return &yamlmeta.Map{Items: items}
 	case *AnyType:
 		var items openAPIKeys
-		items = append(items, j.collectDocumentation(typedValue)...)
+		items = append(items, collectDocumentation(typedValue)...)
 		// AnyType must allow all value types, and need to explicitly list them for json schema
 		items = append(items, &yamlmeta.MapItem{Key: typeProp,
 			Value: &yamlmeta.Array{Items: []*yamlmeta.ArrayItem{
@@ -126,39 +126,5 @@ func (j *JSONSchemaDocument) calculateProperties(schemaVal interface{}) *yamlmet
 		return &yamlmeta.Map{Items: items}
 	default:
 		panic(fmt.Sprintf("Unrecognized type %T", schemaVal))
-	}
-}
-
-func (j *JSONSchemaDocument) collectDocumentation(typedValue Type) []*yamlmeta.MapItem {
-	var items []*yamlmeta.MapItem
-	if typedValue.GetTitle() != "" {
-		items = append(items, &yamlmeta.MapItem{Key: titleProp, Value: typedValue.GetTitle()})
-	}
-	if typedValue.GetDescription() != "" {
-		items = append(items, &yamlmeta.MapItem{Key: descriptionProp, Value: typedValue.GetDescription()})
-	}
-	if isDeprecated, _ := typedValue.IsDeprecated(); isDeprecated {
-		items = append(items, &yamlmeta.MapItem{Key: deprecatedProp, Value: isDeprecated})
-	}
-	examples := typedValue.GetExamples()
-	if len(examples) != 0 {
-		items = append(items, &yamlmeta.MapItem{Key: exampleDescriptionProp, Value: examples[0].description})
-		items = append(items, &yamlmeta.MapItem{Key: exampleProp, Value: examples[0].example})
-	}
-	return items
-}
-
-func (j *JSONSchemaDocument) jsonSchemaTypeFor(astType *ScalarType) string {
-	switch astType.ValueType {
-	case StringType:
-		return "string"
-	case FloatType:
-		return "number"
-	case IntType:
-		return "integer"
-	case BoolType:
-		return "boolean"
-	default:
-		panic(fmt.Sprintf("Unrecognized type: %T", astType.ValueType))
 	}
 }
